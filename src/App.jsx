ba7c2b2fe38f71dad69965a3918ba01689404690
MIREHMAN <Inammalik73@gmail.com>
2024-11-02 22:41:44 +0500

    Dashboard Layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Grid from '@mui/material/Grid2'
+import DashboardLayout from './layouts/DashboardLayout'
+
+function App() {
+  return (
+    <Grid>
+      <DashboardLayout/>
+    </Grid>
+  )
+}
+
+export default App
